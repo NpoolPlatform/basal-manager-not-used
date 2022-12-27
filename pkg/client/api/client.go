e68c1bda5367806e4680116989ba59da9ecdedc7
Zhao KK <kikakkz@hotmail.com>
2022-12-27 02:57:33 +0000

    Correct api client

--- pkg/client/api/client.go
+++ pkg/client/api/client.go
@@ -114,7 +114,7 @@ func GetAPIOnly(ctx context.Context, conds *npool.Conds) (*npool.API, error) {
 	return info.(*npool.API), nil
 }
 
-func GetAPIs(ctx context.Context, conds *npool.Conds, limit, offset int32) ([]*npool.API, uint32, error) {
+func GetAPIs(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.API, uint32, error) {
 	var total uint32
 	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.ManagerClient) (cruder.Any, error) {
 		resp, err := cli.GetAPIs(ctx, &npool.GetAPIsRequest{
